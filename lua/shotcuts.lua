b5f9d294f7d755dfa02198606a4323f6abb6a537
Krysztal <suibing112233@outlook.com>
2023-03-14 22:18:51 +0800

    ✨ Feat: Add leander

--- lua/shotcuts.lua
+++ lua/shotcuts.lua
@@ -19,14 +19,16 @@ MappingUtil.nMap("<C-J>", "<C-W>j")
 MappingUtil.nMap("<C-K>", "<C-W>k")
 MappingUtil.nMap("<C-L>", "<C-W>l")
 
+-- 'w' start its means `window`
 -- Change window size mapping
 MappingUtil.nMap("<leader>wwu", "<cmd>vertical resize +5<cr>")
 MappingUtil.nMap("<leader>wwd", "<cmd>vertical resize -5<cr>")
 MappingUtil.nMap("<leader>whu", "<cmd>resize +5<cr>")
 MappingUtil.nMap("<leader>whd", "<cmd>resize -5<cr>")
 
+-- 'o' start its means `open`
 -- Open tree window
-MappingUtil.nMap("<leader>e", "Ntree<cr>")
+MappingUtil.nMap("<leader>oe", "<cmd>NvimTreeOpen<cr>")
 
 -- Selece mode
 MappingUtil.nMap("<CS-s>", "gh<cr>")
